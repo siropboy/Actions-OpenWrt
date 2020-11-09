9d56f0916835d70aeb3a8b1e5a9c3790cd4eef15
siropboy <64370904+siropboy@users.noreply.github.com>
2020-11-10 02:47:22 +0800

    set

--- diy5.4.sh
+++ diy5.4.sh
@@ -10,35 +10,14 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 sed -i '$a src-git diy https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
-# git clone https://github.com/kenzok8/openwrt-packages package/diy
+sed -i 's/coolsnowwolf/sirpdboy/g' ./feeds.conf.default
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./package/new/smartdns
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome ./package/new/adguardhome
 #git clone https://github.com/sirpdboy/sirpdboy-package package/diy
 #git clone https://github.com/siropboy/luci-app-vssr-plus  package/luci-app-vssr-plus
-# mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-#　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-# git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#svn co https://github.com/firker/diy-ziyong/trunk/smartdns package/smartdns
-#svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-#svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
-# svn co https://github.com/jerrykuku/luci-app-vssr/trunk  ./package/new/luci-app-vssr
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/naiveproxy package/naiveproxy
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray package/v2ray
-# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray-plugin package/v2ray-plugin
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/new/luci-app-vssr
-#svn co https://github.com/siropboy/luci-app-vssr-plus/trunk/ package/new/luci-app-vssr-plus
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
