e12937e55569770cc605eca9584bc6687a9a33a4
siropboy <64370904+siropboy@users.noreply.github.com>
2020-11-09 08:10:01 +0800

    setdiy

--- diy5.4.sh
+++ diy5.4.sh
@@ -9,21 +9,18 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 # git clone https://github.com/kenzok8/openwrt-packages package/diy
-sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-sed -i 's/coolsnowwolf/sirpdboy/g' ./feeds.conf.default
 #git clone https://github.com/sirpdboy/sirpdboy-package package/diy
 #git clone https://github.com/siropboy/luci-app-vssr-plus  package/luci-app-vssr-plus
 # mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 #　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-#git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
@@ -41,8 +38,7 @@ sed -i 's/coolsnowwolf/sirpdboy/g' ./feeds.conf.default
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/naiveproxy package/naiveproxy
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray package/v2ray
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray-plugin package/v2ray-plugin
-
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/new/luci-app-vssr
-svn co https://github.com/siropboy/luci-app-vssr-plus/trunk/luci-app-vssr-plus package/new/luci-app-vssr-plus
+# svn co https://github.com/siropboy/luci-app-vssr-plus/trunk/luci-app-vssr-plus package/new/luci-app-vssr-plus
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
